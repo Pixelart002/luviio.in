7192ca03c31e1564c2557b7c63f9e614aed5f5be
kyro <9013ms@gmail.com>
2026-02-09 15:38:02 +0530

    update

--- routers/auth.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-from config import templates  # Humari config file se
-
-router = APIRouter()
-
-# Note: Jinja2 mein 'request' pass karna zaroori hota hai
-@router.get("/login") 
-async def get_login_page(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
