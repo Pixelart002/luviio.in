523e2c04cf0a64688e75c75fd8f35962334b7775
kyro <9013ms@gmail.com>
2026-02-09 16:54:07 +0530

    update

--- api/main.py
+++ api/main.py
@@ -1,7 +1,12 @@
+# api/main.py
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-async def hello_world():
-    return {"message": "Hello, World!"}
+async def home():
+    return {"message": "Hello FastAPI!"}
+
+@app.get("/pe")
+async def pe():
+    return {"message": "This is /pe route"}
