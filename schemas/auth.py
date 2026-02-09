2c4266954d886f3df44060a2c538be5140b18c25
kyro <9013ms@gmail.com>
2026-02-09 13:28:36 +0530

    update

--- schemas/auth.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-
-class AuthSchema(BaseModel):
-    email: EmailStr
-    password: str = Field(..., min_length=6)
-
-class SignupSchema(AuthSchema):
-    # Add extra fields for signup if needed
-    pass
