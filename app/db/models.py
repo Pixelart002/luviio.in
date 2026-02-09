d9aac6f0fa87dd86614aca02ba627e024958d31a
kyro <9013ms@gmail.com>
2026-02-09 13:01:55 +0530

    update

--- app/db/models.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class Product(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    name: str
-    description: str
-    price: float
-    stock: int
-    image_url: str
