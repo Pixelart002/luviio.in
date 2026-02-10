0dad8c3811aac5d2737281721b7c3cabfba00a29
kyro <9013ms@gmail.com>
2026-02-11 00:40:52 +0530

    update

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -6,11 +6,8 @@ router = APIRouter()
 @router.get("/", response_class=HTMLResponse)
 async def render_home(request: Request, x_up_target: str = Header(None)):
     templates = request.app.state.templates
-    
     return templates.TemplateResponse("app/pages/home.html", {
         "request": request,
-        "title": "Luviio - The Trust Layer for Marketplaces",
-        "description": "Verified operating system for online listings.",
-        "active_page": "home",
-        "up_fragment": x_up_target is not None
+        "up_fragment": x_up_target is not None,
+        "active_page": "home"
     })
