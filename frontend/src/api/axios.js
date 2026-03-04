1943d12ba2fb00db931be9bbf9224d0fa33aafa0
kyro <9013ms@gmail.com>
2026-03-04 08:12:36 +0530

    update

--- frontend/src/api/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    // VITE_BACKEND_URL hum baad me set karenge
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
-    withCredentials: true, // Cookies frontend se backend bhejne ke liye VERY IMPORTANT
-});
