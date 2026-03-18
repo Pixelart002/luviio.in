1577f5a98add8397fdf20a9b0977b05db18a16d5
kyro <9013ms@gmail.com>
2026-03-18 11:35:54 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -105,7 +105,7 @@ app.add_middleware(
     allow_origins=settings.cors_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type", "stripe-signature"],
+    allow_headers=["*","Authorization", "Content-Type", "stripe-signature"],
 )
 
 app.state.limiter = limiter
