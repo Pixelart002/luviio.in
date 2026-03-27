7e3b0c9e5fc8ef1fa6628106b0fb893c5c0b2b07
kyro <9013ms@gmail.com>
2026-03-27 21:09:52 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -68,8 +68,8 @@ app = FastAPI(
     title=settings.APP_NAME,
     version="1.0.0",
     docs_url="/docs",
-    redoc_url="/redoc" if not settings.is_production else None,
-    openapi_url="/openapi.json" if not settings.is_production else None,
+    redoc_url="/redoc",
+    openapi_url="/openapi.json",
     lifespan=lifespan,
 )
 
