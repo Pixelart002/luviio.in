c21530f04fa639d0cc74ec6dba4d03e620a3bc1a
kyro <9013ms@gmail.com>
2026-03-27 20:59:02 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -67,7 +67,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 app = FastAPI(
     title=settings.APP_NAME,
     version="1.0.0",
-    docs_url="/docs" if not settings.is_production else None,
+    docs_url="/docs"## if not settings.is_production else None,
     redoc_url="/redoc" if not settings.is_production else None,
     openapi_url="/openapi.json" if not settings.is_production else None,
     lifespan=lifespan,
