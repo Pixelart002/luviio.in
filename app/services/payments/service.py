fa3c4a64c72c93054848c316be7f3f6e2ba2a507
kyor <lol>
2026-07-07 08:18:10 +0530

    update service.py

--- app/services/payments/service.py
+++ app/services/payments/service.py
@@ -74,7 +74,7 @@ class PaymentService:
                 "product_id": item["product_id"], 
                 "product_name": prod.get("name", "Item"),
                 "unit_price": float(locked_price),
-                "compare_price": float(prod.get("compare_price"),
+                "compare_price": float(prod.get("compare_price")),
                 "quantity": item["quantity"], 
                 "subtotal": float(lt)
             })
