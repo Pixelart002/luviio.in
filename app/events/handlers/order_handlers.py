9ffc718ef39de41a4c6495d1528e45fc23574ea8
kyor <lol>
2026-08-05 10:52:49 +0530

    update order_handlers.py module errors

--- app/events/handlers/order_handlers.py
+++ app/events/handlers/order_handlers.py
@@ -9,7 +9,7 @@ import os
 import logging
 from typing import Any
 
-from app.services.events import (
+from app.services.bus import (
     OrderCreatedEvent, OrderPaidEvent, OrderFailedEvent, 
     OrderShippedEvent, OrderStatusChangedEvent, LowStockEvent
 )
